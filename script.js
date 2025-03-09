423cfdb3bf1fe903c8895fc12bb268e413f670d7
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-09 16:54:06 +0100

    Display results on the UI

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttons = document.querySelectorAll("button");
+const roundResultText = document.querySelector("#roundResult");
+const humanScoreText = document.querySelector("#humanScore");
+const computerScoreText = document.querySelector("#computerScore");
+const finalResultText = document.querySelector("#finalResult");
 let humanScore = 0;
 let computerScore = 0;
 
@@ -9,41 +13,43 @@ buttons.forEach(button => {
 });
 
 function playRound(humanChoice, computerChoice) {
+  if (finalResultText.textContent !== "") {
+    finalResultText.textContent = "";
+  }
   if (humanChoice === "rock") {
     if (computerChoice === "rock") {
-      console.log("It's a draw!");
+      roundResultText.textContent = "It's a draw!";
     } else if (computerChoice === "paper") {
-      console.log("You lose! Paper beats rock.");
+      roundResultText.textContent = "You lose! Paper beats rock.";
       computerScore++;
     } else {
-      console.log("You win! Rock beats scissors.");
+      roundResultText.textContent = "You win! Rock beats scissors.";
       humanScore++;
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
-      console.log("You win! Paper beats rock.");
+      roundResultText.textContent = "You win! Paper beats rock.";
       humanScore++;
     } else if (computerChoice === "paper") {
-      console.log("It's a draw!");
+      roundResultText.textContent = "It's a draw!";
     } else {
-      console.log("You lose! Scissors beats paper.");
+      roundResultText.textContent = "You lose! Scissors beats paper.";
       computerScore++;
     }
   } else {
     if (computerChoice === "rock") {
-      console.log("You lose! Rock beats scissors.");
+      roundResultText.textContent = "You lose! Rock beats scissors.";
       computerScore++;
     } else if (computerChoice === "paper") {
-      console.log("You win! Scissors beats paper.");
+      roundResultText.textContent = "You win! Scissors beats paper.";
       humanScore++;
     } else {
-      console.log("It's a draw.");
+      roundResultText.textContent = "It's a draw!";
     }
   }
-  console.log(`human: ${humanScore}`);
-  console.log(`computer: ${computerScore}`);
+  humanScoreText.textContent = `human: ${humanScore}`;
+  computerScoreText.textContent = `computer: ${computerScore}`;
   checkWinner();
-
 }
 
 function getComputerChoice() {
@@ -59,11 +65,11 @@ function getComputerChoice() {
 
 function checkWinner() {
   if (humanScore === 5) {
-    console.log("You're the winner!");
+    finalResultText.textContent = "You're the winner!";
     humanScore = 0;
     computerScore = 0;
   } else if (computerScore === 5) {
-    console.log("The computer is the winner!");
+    finalResultText.textContent = "The computer is the winner!";
     humanScore = 0;
     computerScore = 0;
   }
